29db8263dfdb8ecd58d31ed129e6b8bbe634473c
hitmachut <hitmachut@SHAKED-05>
2019-06-17 17:32:08 +0300

    test vs

--- BLL/UserFunctions.cs
+++ BLL/UserFunctions.cs
@@ -12,10 +12,10 @@ namespace BLL
     {
         static AshavatAvedaEntities db = new AshavatAvedaEntities();
         //null פונקצית כניסה האם המשתמש קים ומחזירה את המשתמש שנמצא או 
-        public static DTO.UserDTO Login(string userRecognizeName, string password)
+        public static DTO.UserDTO Login(string UserPhone)
         {
 
-            DAL.User u1 = db.Users.FirstOrDefault(u => u.UserRecognizeName == userRecognizeName && u.UserPassword == password);
+            DAL.User u1 = db.Users.FirstOrDefault(u => u.UserPhone == UserPhone );
             if (u1 != null)
             {
                 return BLL.Convertions.UserToDto(u1);
@@ -27,7 +27,7 @@ namespace BLL
         public static DTO.UserDTO Register(DTO.UserDTO user)
         {
             DAL.User u1 = BLL.Convertions.UserDtoToDAL(user);
-            if (Login(u1.UserRecognizeName, u1.UserPassword) == null)
+            if (Login(u1.UserPhone) == null)
             {
                 u1.RoleId = 3;
                 db.Users.Add(u1);
@@ -56,6 +56,41 @@ namespace BLL
             }
             return null;
         }
-       
+        //פונקציה ששומרת את השינויים על עריכת משתמש
+       public static void UpdateEditUser(DTO.UserDTO user)
+        {
+            DAL.User u1 = BLL.Convertions.UserDtoToDAL(user);
+           DAL.User u2 = db.Users.FirstOrDefault(u => u.UserId == u1.UserId);
+            if (u2!=null)
+            {
+            u2.UserPassword = u1.UserPassword;
+            u2.UserPhone = u1.UserPhone;
+            u2.UserRecognizeName = u1.UserRecognizeName;
+            u2.UserFullName = u1.UserFullName;
+            u2.UserEmail = u1.UserEmail;
+            u2.UserAddress = u1.UserAddress;
+            u2.RoleId = u1.RoleId;
+            db.SaveChanges();
+            }
+
+        }
+        //פונקציה שמוסיפה משתמש או מוקדן
+        //public static DTO.UserDTO AddMokdanOrUser(DTO.UserDTO user)
+        //{
+        //    DAL.User u1 = BLL.Convertions.UserDtoToDAL(user);
+        //    if (Login(u1.UserRecognizeName, u1.UserPassword) == null)
+        //    {
+        //        u1.RoleId = 3;
+        //        db.Users.Add(u1);
+        //        db.SaveChanges();
+        //        return BLL.Convertions.UserToDto(u1);
+
+        //    }
+        //    else
+        //    {
+        //        return null;
+        //    }
+
+        //}
     }
 }
--- WebService/Controllers/UserController.cs
+++ WebService/Controllers/UserController.cs
@@ -16,9 +16,9 @@ namespace WebService.Controllers
         //Login
         [Route("Login")]
         [HttpGet]
-        public IHttpActionResult Login(string userRecognizeName, string password)
+        public IHttpActionResult Login(string UserPhone)
         {
-            UserDTO u = BLL.UserFunctions.Login(userRecognizeName, password);
+            UserDTO u = BLL.UserFunctions.Login(UserPhone);
             if (u != null)
                 return Ok(u);
             return BadRequest("the user isnt exist!");
@@ -43,6 +43,24 @@ namespace WebService.Controllers
             if (u != null)
                 return Ok(u);
             return BadRequest("there aren't users!"); ;
+        }
+        //UpdateEditUser
+        [Route("UpdateEditUser")]
+        [HttpPost]
+        public void UpdateEditUser(UserDTO user)
+        {
+            BLL.UserFunctions.UpdateEditUser(user);
+        }
+        //AddMokdanOrUser
+        [Route ("AddMokdanOrUser")]
+        [HttpPost]
+        public IHttpActionResult AddMokdanOrUser(UserDTO user)
+        {
+            //user.RoleId = 3;
+            UserDTO u = BLL.UserFunctions.AddMokdanOrUser(user);
+            if (u != null)
+                return Ok(u);
+            return BadRequest("the user is already exist!");
         }
     }
 }
